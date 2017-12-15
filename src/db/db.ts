edddc3b37c5685ef3692c7120a345f0aa0b6b5e8
Carlos Precioso <kalprestito@gmail.com>
2017-12-15 15:33:44 +0100

    Handle process.env better

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,20 @@
 import * as admin from "firebase-admin"
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN)
+const accountCertificate = {
+  type: process.env.FIREBASE_KEY_TYPE,
+  project_id: process.env.FIREBASE_KEY_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_KEY_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_KEY_PRIVATE_KEY.replace(/\\n/giu, "\n"),
+  client_email: process.env.FIREBASE_KEY_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_KEY_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_KEY_AUTH_URI,
+  token_uri: process.env.FIREBASE_KEY_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_KEY_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_KEY_CLIENT_X509_CERT_URL,
+} as admin.ServiceAccount
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(accountCertificate),
   databaseURL: process.env.FIREBASE_DB
 })
 
